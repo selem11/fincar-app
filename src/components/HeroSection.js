3d713eb90b7b773c65427376b10a68d9d31112cb
Nelcy Abril Murillo <nelcyabrilmurillo@gmail.com>
2021-11-03 08:33:44 -0500

    video using firebase

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,9 +3,11 @@ import { Button } from './Button';
 
 import './heroSection.css';
 
-import video3 from '../assets/videos/video-3.mp4';
+// import video3 from '../assets/videos/video-3.mp4';
 
 // import App.css if you need it
+
+const video3 = "https://firebasestorage.googleapis.com/v0/b/fincar-app-f2601.appspot.com/o/video-3.mp4?alt=media&token=9c525440-4a75-4eba-9850-1712101546d6";
 
 const HeroSection = () => {
     return (
